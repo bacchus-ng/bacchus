44c781bbc0b01b9d1ac5dc093a10b667b2f7a809
Niyazi Elvan <niyazielvan@gmail.com>
2017-04-27 21:02:51 +0300

    Modify schedule functionality completed

--- scheduler/views.py
+++ scheduler/views.py
@@ -97,18 +97,75 @@ def delete_sched(request):
 
 @login_required(login_url="/login/")
 def edit_sched(request, sched_id):
-       
-    sched = Schedule.objects.get(id=sched_id)
-    minutes = range(0,60)
-    minutes.insert(0,'*')
-    hours = range(0,24)
-    hours.insert(0,'*')
-    days = range(1,32)
-    days.insert(0,'*')
-    months = range(1,13)
-    months.insert(0,'*')
-    
-    return render(request,'editsched.html',{'sched': sched,'minutes': minutes, 'hours': hours, 'days':days, 'months':months })
+    if request.method == "GET":               
+        sched = Schedule.objects.get(id=sched_id)
+        minutes = range(0,60)
+        minutes.insert(0,'*')
+        hours = range(0,24)
+        hours.insert(0,'*')
+        days = range(1,32)
+        days.insert(0,'*')
+        months = range(1,13)
+        months.insert(0,'*')
+        weekdays = range(0,7)
+        weekdays.insert(0, '*')
+        vms = VM.objects.all()
+        backupvms = []
+        backupscheds = BackupSchedule.objects.all().filter(schedule=sched)
+        for backupsched in backupscheds:
+            backupvms.append(backupsched.vm)
+            
+        return render(request,'editsched.html',{'vms':vms,'backupvms':backupvms,'sched': sched,'minutes': minutes, 'hours': hours, 'days':days, 'months':months, 'weekdays':weekdays })
+    else:
+        sched_id = request.POST.get('sched_id')
+        sched = Schedule.objects.get(id=sched_id)
+
+        # invalidate current backup schedules
+        backupscheds = BackupSchedule.objects.all().filter(schedule=sched)
+        for backupsched in backupscheds:
+            backupsched.delete()
+
+        # do not miss any change, assign all variables    
+        sched_name = request.POST.get('sched_name')
+        sched_min = request.POST.get('sched_min')
+        sched_hour = request.POST.get('sched_hour')
+        sched_week = request.POST.get('sched_week')
+        sched_month = request.POST.get('sched_month')
+        sched_year = request.POST.get('sched_year')
+
+        # change the values in bacchus db
+        sched_old_name = sched.name
+        sched.name = sched_name
+        sched.minute = sched_min
+        sched.hour = sched_hour
+        sched.week = sched_week
+        sched.month = sched_month
+        sched.year = sched_year
+        sched.save()
+        
+        # get celery beat object
+        celery_sched = CrontabSchedule.objects.get(id=sched.celery_sched_id)
+        celery_sched.minute=sched_min
+        celery_sched.hour=sched_hour
+        celery_sched.day_of_week=sched_week
+        celery_sched.day_of_month=sched_month
+        celery_sched.month_of_year=sched_year
+        celery_sched.save()
+        
+        celery_task = PeriodicTask.objects.get(name=sched_old_name)
+        celery_task.name = sched.name
+        celery_task.save()
+        
+        # now get the vm list and re-schedule backups
+        vmlist = request.POST.getlist('schedvmlist')
+        for vmid in vmlist:
+            vm = VM.objects.get(vmid=vmid)
+            backupsched = BackupSchedule(vm=vm,schedule=sched)
+            backupsched.save()
+        
+        CeleryTools.restart_beat()      
+            
+        return redirect('/schedules/')
 
 
 
