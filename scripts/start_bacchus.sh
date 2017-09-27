497a8f622710124a548f26c8473175b437cb30a6
Brett Maton <matonb@ltresources.co.uk>
2017-09-27 18:05:26 +0100

    Log paths are also relative to scripts.
    Fix log path for 'execute anywhere'

--- scripts/start_bacchus.sh
+++ scripts/start_bacchus.sh
@@ -1,6 +1,8 @@
 SCRIPTPATH=$(cd $(dirname $0) ; pwd -P)
-if [ ! -d logs ]; then
-mkdir logs
+PARENTPATH=$(dirname $SCRIPTPATH)
+HOMEPATH=$(dirname $PARENTPATH)
+if [ ! -d ${HOMEPATH}/logs ]; then
+mkdir ${HOMEPATH}/logs
 fi 
 ${SCRIPTPATH}/start_celery.sh
 ${SCRIPTPATH}/start_server.sh
