ef69c7023369c2c7aef798a8dd1ae806f685faee
Brett Maton <matonb@ltresources.co.uk>
2017-09-27 17:11:55 +0100

    Fix log path for 'scripts anywhere'

--- scripts/flower.sh
+++ scripts/flower.sh
@@ -1,3 +1,4 @@
 SCRIPTPATH=$(cd $(dirname $0) ; pwd -P)
 PARENTPATH=$(dirname $SCRIPTPATH)
-nohup celery -A bacchus flower --broker=amqp://bacchus:bacchus@localhost:5672/bacchus --basic_auth=admin:Passw0rd --workdir ${PARENTPATH}/ >logs/flower.log 2>&1 &
+HOMEPATH=$(dirname $PARENTPATH)
+nohup celery -A bacchus flower --broker=amqp://bacchus:bacchus@localhost:5672/bacchus --basic_auth=admin:Passw0rd --workdir ${PARENTPATH}/ >${HOMEPATH}/logs/flower.log 2>&1 &
--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,4 +1,5 @@
 SCRIPTPATH=$(cd $(dirname $0) ; pwd -P)
 PARENTPATH=$(dirname $SCRIPTPATH)
-nohup celery -A bacchus worker -l info --concurrency=16 -b amqp://bacchus:bacchus@localhost:5672/bacchus --workdir ${PARENTPATH}/ >logs/worker.log 2>&1 &
+HOMEPATH=$(dirname $PARENTPATH)
+nohup celery -A bacchus worker -l info --concurrency=16 -b amqp://bacchus:bacchus@localhost:5672/bacchus --workdir ${PARENTPATH}/ >${HOMEPATH}/logs/worker.log 2>&1 &
 
