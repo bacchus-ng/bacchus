cdfd49161fc7357e7a00b1c1b489e6beb129de1b
bacchus <bacchus@openbacchus.com>
2018-02-24 14:05:27 +0300

    settings save

--- settings/views.py
+++ settings/views.py
@@ -11,12 +11,15 @@ from models import *
 
 @login_required(login_url="/settings/")
 def settings(request):
+    settings_saved = False
     if request.method == "POST":
         settings = Settings.objects.all()
         for setting in settings:
             setting.value = request.POST[setting.parameter]
             setting.save()
+        settings_saved = True
+        
             
     settings = Settings.objects.all()
-    return render(request,'settings.html',{'settings': settings })
+    return render(request,'settings.html',{'settings': settings, 'settings_saved': settings_saved })
 
