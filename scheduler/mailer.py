cdb29327954dc633154e32cb688c9997116f99d6
Niyazi Elvan <niyazielvan@gmail.com>
2018-02-25 21:40:42 +0200

    mailer

---
+++ scheduler/mailer.py
@@ -0,0 +1,20 @@
+from django.core.mail import send_mail
+from settings.models import Settings
+
+class MailTools:
+    @staticmethod
+    def sendMail(mail_to,message):
+        return send_mail("Open Bacchus Notification",message,'',mail_to,fail_silently=False,)
+    
+    @staticmethod
+    def testMail():
+        mails = Settings.objects.get(parameter="mail_to")
+        mail_to = mails.value.split(',')        
+        return MailTools.sendMail(mail_to,"This is an auto-generated message to test Open Bacchus e-mail settings.")
+
+    @staticmethod
+    def notifyUsers(message):
+        mails = Settings.objects.get(parameter="mail_to")
+        mail_to = mails.value.split(',')        
+        return MailTools.sendMail(mail_to,message)
+        
